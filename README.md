6936ac9591a19ac5da7608ad75e0072f90e2f058
Jake Soenneker <jake@soenneker.com>
2023-05-26 10:35:33 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Utils.Test.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Utils.Test/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.utils.test/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.utils.test/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Utils.Test.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Utils.Test/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Utils.Test
+### A utility library for useful test-based operations
+
+## Installation
+
+```
+Install-Package Soenneker.Utils.Test
+```
---
+++ src/TestUtil.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Soenneker.Utils.Test;
+
+/// <summary>
+/// A utility library for useful test-based operations
+/// </summary>
+public class TestUtil
+{
+    /// <summary>
+    /// Builds and returns an <see cref="IConfiguration"/> from appsettings.json in the current directory (optionally plus a child path if there are multiple appsettings needed)
+    /// </summary>
+    public static IConfiguration BuildConfig(string? childPath = null)
+    {
+        string directory;
+        
+        if (childPath != null)
+            directory = Path.Combine(Directory.GetCurrentDirectory(), childPath);
+        else
+            directory = Directory.GetCurrentDirectory();
+
+        const string baseAppSettings = "appsettings.json";
+        string? environmentAppSettings = null;
+
+        IConfigurationBuilder builder = new ConfigurationBuilder()
+            .SetBasePath(directory)
+            .AddJsonFile(baseAppSettings);
+
+        if (environmentAppSettings != null)
+            builder.AddJsonFile(environmentAppSettings);
+
+        IConfiguration configRoot = builder.Build();
+
+        return configRoot;
+    }
+}
